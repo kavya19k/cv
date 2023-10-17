e88633170c8d3b043e93173b6a407036dd1f6021
kavya19k <115345046+kavya19k@users.noreply.github.com>
2023-10-17 20:52:54 +0530

    Add files via upload

--- equ_hist.py
+++ equ_hist.py
@@ -1,22 +1,28 @@
-from PIL import Image
-import numpy as np
+import cv2
 import matplotlib.pyplot as plt
-image =Image.open('img.png')
-grayscale_image = image.convert("L")
-pixel_matrix = np.array(grayscale_image)
-histogram, bins = np.histogram(pixel_matrix, bins=256, range=(0, 256))
-cdf = histogram.cumsum()
-cdf_normalized = ((cdf - cdf.min()) * 255) / (cdf.max() - cdf.min())
-equalization_lookup = np.interp(pixel_matrix, bins[:-1], cdf_normalized)
-equalized_image = Image.fromarray(equalization_lookup.astype('uint8'))
-equalized_histogram, _ = np.histogram(equalization_lookup, bins=256, range=(0, 256))
-plt.figure(figsize=(10, 4))
-plt.subplot(1, 2, 1)
-plt.title("Equalized Image")
-plt.imshow(equalized_image, cmap='gray')
-plt.subplot(1, 2, 2)
-plt.title("Equalized Histogram")
-plt.xlabel("Pixel Value")
-plt.ylabel("Frequency")
-plt.bar(range(256), equalized_histogram)
-plt.show()
+
+def histogram(pixel_matrix, bins=256, range=(0, 256)):
+    histogram = [0] * bins
+    min_range, max_range = range
+
+    for row in pixel_matrix:
+        for pixel_value in row:
+            if min_range <= pixel_value < max_range:
+                bin_index = int((pixel_value - min_range) / (max_range - min_range) * bins)
+                histogram[bin_index] += 1
+
+    return histogram, list(range(min_range, max_range, int((max_range - min_range) / bins)))
+
+def main():
+    # Load an image
+    image = cv2.imread("img.png", cv2.IMREAD_GRAYSCALE)
+
+    # Calculate the histogram
+    hist, bin_edges = histogram(image, bins=256, range=(0, 256))
+
+    # Display the original image
+    plt.subplot(121)
+    plt.imshow(image, cmap='gray')
+    plt.title('Original Image')
+
+    # Display the
